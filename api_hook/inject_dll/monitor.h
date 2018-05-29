76b4d83e0f00796b19f96802713de1d88216eb52
Janaury <january147@outlook.com>
2018-05-29 14:39:13 +0800

    添加hook进程监控程序

---
+++ api_hook/inject_dll/monitor.h
@@ -0,0 +1,32 @@
+
+FARPROC myProcess(HMODULE hmodule, LPCSTR lpProcName);
+
+BOOL mycreateProcessW(
+	LPCWSTR lpApplicationName,
+	LPWSTR lpCommandLine,
+	LPSECURITY_ATTRIBUTES lpProcessAttributes,
+	LPSECURITY_ATTRIBUTES lpThreadAttributes,
+	BOOL bInheritHandles,
+	DWORD dwCreationFlags,
+	LPVOID lpEnvironment,
+	LPCWSTR lpCurrentDirectory,
+	LPSTARTUPINFOW lpStartupInfo,
+	LPPROCESS_INFORMATION lpProcessInformation
+);
+
+BOOL mycreateProcessA(
+	LPCSTR lpApplicationName,
+	LPSTR lpCommandLine,
+	LPSECURITY_ATTRIBUTES lpProcessAttributes,
+	LPSECURITY_ATTRIBUTES lpThreadAttributes,
+	BOOL bInheritHandles,
+	DWORD dwCreationFlags,
+	LPVOID lpEnvironment,
+	LPCSTR lpCurrentDirectory,
+	LPSTARTUPINFOA lpStartupInfo,
+	LPPROCESS_INFORMATION lpProcessInformation
+);
+
+BOOL initHook(char* workpath);
+
+void pathcCreateProcess(void* org_func_addr, const char* org_func_name);
